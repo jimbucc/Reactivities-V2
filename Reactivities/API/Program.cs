8a8e72bec049579fa9bf6c4ecee7158680c646b1
jimbucc <James.Bucciferro@PERKINELMER.COM>
2025-04-07 19:17:24 -0400

    End of Section 3

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<AppDbContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +21,10 @@ if (app.Environment.IsDevelopment())
 {
 
 }
+
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
---
+++ Reactivities/client/src/lib/types/index.d.ts
@@ -0,0 +1,11 @@
+type Activity = {
+  id: string
+  title: string
+  date: string
+  description: string
+  category: string
+  isCancelled: boolean
+  city: string
+  venue: string
+  latitude: number
+}
