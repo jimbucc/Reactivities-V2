0c11886287a213752fe0c78bbed8bb50718d0353
jimbucc <James.Bucciferro@PERKINELMER.COM>
2025-04-08 08:52:26 -0400

    End of Section 4

--- Reactivities/API/Controllers/BaseAPIController.cs
+++ Reactivities/API/Controllers/BaseAPIController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,5 +8,12 @@ namespace API.Controllers
     [ApiController]
     public class BaseAPIController : ControllerBase
     {
+        private IMediator? _mediator;
+
+        // To avoid having to inject the MediatR service, create it here and
+        // make it available to all base classes.
+        protected IMediator Mediator => 
+            _mediator ??= HttpContext.RequestServices.GetService<IMediator>()
+            ?? throw new InvalidOperationException("IMediator service is unavailable");
     }
 }
